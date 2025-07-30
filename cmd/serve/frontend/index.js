4eec008d6958a205a2384158ab6ee5558510bfca
imago <l.kinde94@outlook.com>
2025-07-30 21:08:49 +0300

    feat: Sort selection input box, remember previous playtime

--- cmd/serve/frontend/index.js
+++ cmd/serve/frontend/index.js
@@ -4,6 +4,7 @@ fetch('/gallery')
   .then(response => response.json())
   .then(data => {
     const options = document.getElementById("debugMediaSelector")
+    data.sort((a, b) => a.Name.localeCompare(b.Name))
     for (const i of data) {
       if (!i.MIMEType.includes("video")) {
         continue
@@ -51,3 +52,25 @@ function selectSubtitle(id) {
   const track = document.getElementById("subs");
   track.src = `/gallery/subs/${mostRecentID}/${id}`;
 }
+
+setTimeout(() => {
+  const screen = document.getElementById("screen")
+  screen.addEventListener("timeupdate", function () {
+    localStorage.setItem(
+      "video_play_duration_" + mostRecentID,
+      this.currentTime
+    );
+  });
+
+
+  screen.addEventListener("loadeddata", function () {
+    const playTime = localStorage.getItem(
+      "video_play_duration_" + mostRecentID
+    );
+    if (playTime) {
+      screen.currentTime = playTime
+    }
+
+  });
+}, 10)
+
